80fb6672083c3c8587b2a8a4aa1808c66dfb799d
Sai Praveen Kondapalli <63105626+saipraveenkondapalli@users.noreply.github.com>
2024-06-12 10:32:38 +0530

    sep api endpoints and normal view endpoints

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -1,10 +1,8 @@
 from django.urls import path
 
-from .apis import ContactCreateAPIView, ResumeApiView
 from .views import dashboard
 
 urlpatterns = [
-    path('api/contact/', ContactCreateAPIView.as_view(), name='contact'),
-    path('api/resume/', ResumeApiView.as_view(), name='resume'),
-    path('dashboard/', dashboard, name='dashboard')
+    path('dashboard/', dashboard, name='dashboard'),
+
 ]
